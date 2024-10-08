d8da5d7d551a190bdcf7158727c6c65776237a3c
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-10-08 11:16:34 +0200

    Completed function getBooks()

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -18,7 +18,12 @@ export const addNewBook = async (newTitle, newStart, newEnd) => {
     return await response.json();
 }
 
-// TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
+// Create the getBooks function that retrieves all of the books that have been saved to the back-end API
+export const getBooks = async () => {
+    const response = await fetch(`${API_ENDPOINT}/books`);
+    // Return all books
+    return await response.json();
+}
 
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
 
